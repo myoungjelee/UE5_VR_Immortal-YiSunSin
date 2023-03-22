b36224991ab26ca6a530095ed851938da211b919
myoungjelee <mjzizou@naver.com>
2023-03-22 13:22:58 +0900

    .

---
+++ Source/Immortal_YiSunSin/Public/RhythmBarWidget.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "RhythmBarWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IMMORTAL_YISUNSIN_API URhythmBarWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+
+	virtual void NativeConstruct() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	class UProgressBar* gaugeBar;
+
+	void UpdateGauge(float curr, float max);
+};
