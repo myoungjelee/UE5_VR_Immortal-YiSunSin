64d6b3a805076c3bd97ed0c12c67b9826c3eb01a
myoungjelee <mjzizou@naver.com>
2023-04-07 11:44:42 +0900

    .

---
+++ Source/Immortal_YiSunSin/Private/ShipMoving.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShipMoving.h"
+#include <Components/BoxComponent.h>
+
+// Sets default values
+AShipMoving::AShipMoving()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	box = CreateDefaultSubobject<UBoxComponent>("Box");
+	SetRootComponent(box);
+
+	mesh = CreateDefaultSubobject<UStaticMeshComponent>("StaticMesh");
+	mesh->SetupAttachment(box);
+}
+
+// Called when the game starts or when spawned
+void AShipMoving::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AShipMoving::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	float roll = FMath::Sin(GetGameTimeSinceCreation()) * 5;
+	FRotator rot = FRotator(0.0f, 0.0f, roll);
+	SetActorRotation(rot);
+}
+
